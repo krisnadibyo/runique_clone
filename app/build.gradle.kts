4a7f90696303db9ed05f7ddae2fb3add35039536
krisnadibyo <krisna_itbstei@hotmail.com>
2025-05-23 08:58:10 +0700

    update on build convention

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     alias(libs.plugins.mapsplatform.secrets.plugin)
+    id("runique.android.application")
 }
 
 android {
