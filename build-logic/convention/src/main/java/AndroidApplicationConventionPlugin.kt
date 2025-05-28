8a9daaa53e879a25274765b3e83711636034d159
krisnadibyo <krisna_itbstei@hotmail.com>
2025-05-28 17:15:47 +0700

    build type configuration

--- build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -1,4 +1,6 @@
 import com.android.build.api.dsl.ApplicationExtension
+import com.plcoding.convention.ExtensionType
+import com.plcoding.convention.configureBuildTypes
 import com.plcoding.convention.configureKotlinAndroid
 import com.plcoding.convention.libs
 import org.gradle.api.Plugin
@@ -24,6 +26,8 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
 
                 }
                 configureKotlinAndroid(this)
+                configureBuildTypes(this, ExtensionType.APPLICATION)
+
             }
         }
     }
--- build-logic/convention/src/main/java/com/plcoding/convention/Kotlin.kt
+++ build-logic/convention/src/main/java/com/plcoding/convention/Kotlin.kt
@@ -1,7 +1,6 @@
 package com.plcoding.convention
 
 import com.android.build.api.dsl.CommonExtension
-import com.android.build.gradle.internal.dsl.CommonExtensionImpl
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
