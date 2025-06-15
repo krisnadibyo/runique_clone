d59bd5125f0be8f8cf2ad844903004060d7e31f3
krisnadibyo <krisna_itbstei@hotmail.com>
2025-06-15 12:15:35 +0700

    setup domain core error handling wrapper

---
+++ core/domain/src/main/java/com/plcoding/core/domain/util/DataError.kt
@@ -0,0 +1,21 @@
+package com.plcoding.core.domain.util
+
+sealed interface DataError: Error {
+
+    enum class Network: DataError {
+        REQUEST_TIMEOUT,
+        UNAUTHORIZED,
+        CONFLICT,
+        TOO_MANY_REQUEST,
+        NO_INTERNET,
+        PAYLOAD_TOO_LARGE,
+        SERVER_ERROR,
+        SERIALIZATION,
+        UNKNOWN
+    }
+
+    enum class Local: DataError {
+        DISK_FULL,
+
+    }
+}
